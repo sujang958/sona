7deaebfe4a845fe46415812d69e158b88fd86460
sujang.958 <enter7377@naver.com>
2023-02-28 22:40:26 +0900

    feat: add a modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,62 @@
 <script lang="ts">
   import { onMount } from "svelte"
   import SoundItem from "./lib/SoundItem.svelte"
+  import { appDataDir, join } from "@tauri-apps/api/path"
+  import { writeFile } from "@tauri-apps/api/fs"
 
   let audioFileSelector: HTMLInputElement
 
-  onMount(() => {
+  onMount(async () => {
     const fileReader = new FileReader()
 
     audioFileSelector.addEventListener("change", () => {
       fileReader.readAsDataURL(audioFileSelector.files[0])
-      fileReader.addEventListener("loadend", (event) => {
-        console.log(event.target.result)
+      fileReader.addEventListener("loadend", async (event) => {
+        const name = prompt("Name")
+
+        console.log(name)
+
+        const appDataDirPath = await appDataDir()
+        // todo: store event.target.result in appdata path and maybe make name unique per an audio?
       })
     })
   })
 </script>
+
+<div class="fixed bg-black/50 w-full h-screen z-50 grid place-items-center">
+  <div class="bg-black text-white flex flex-col rounded-lg px-10 py-6 gap-y-3">
+    <p class="text-3xl font-bold">One More Step</p>
+    <div class="pt-0.5" />
+    <label class="py-1 w-full">
+      <p class="text-lg font-semibold">Name</p>
+      <input
+        type="text"
+        class="bg-black rounded-lg border-b border-gray-800 px-3 py-1 outline-none mt-1.5"
+        placeholder="Name"
+      />
+    </label>
+    <label class="py-1 w-full">
+      <p class="text-lg font-semibold">Keybind</p>
+      <input
+        type="text"
+        class="bg-black rounded-lg border-b border-gray-800 px-3 py-1 outline-none mt-1.5 cursor-pointer"
+        placeholder="Click hero to bind keys"
+        disabled
+      />
+    </label>
+    <div class="py-3" />
+    <div class="flex flex-row items-center justify-end w-full gap-x-4">
+      <button
+        class="transition duration-200 hover:bg-red-700 border border-red-700 rounded-lg px-4 py-1"
+        >Cancel</button
+      >
+      <button
+        class="transition duration-200 hover:bg-blue-700 border border-blue-700 rounded-lg px-4 py-1"
+        >Add</button
+      >
+    </div>
+  </div>
+</div>
 
 <input
   type="file"
